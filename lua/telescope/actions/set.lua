807344f499c0e61a03a1d2f374a18a27124f6a73
Jak Crow <jak@jxcrw.io>
2023-03-19 02:41:08 -0700

    hack: Set default preview scroll speed to full page - 3

--- lua/telescope/actions/set.lua
+++ lua/telescope/actions/set.lua
@@ -186,7 +186,7 @@ action_set.scroll_previewer = function(prompt_bufnr, direction)
     return
   end
 
-  local default_speed = vim.api.nvim_win_get_height(status.preview_win) / 2
+  local default_speed = vim.api.nvim_win_get_height(status.preview_win) - 3
   local speed = status.picker.layout_config.scroll_speed or default_speed
 
   previewer:scroll_fn(math.floor(speed * direction))
@@ -200,7 +200,7 @@ end
 --      Valid directions include: "1", "-1"
 action_set.scroll_results = function(prompt_bufnr, direction)
   local status = state.get_status(prompt_bufnr)
-  local default_speed = vim.api.nvim_win_get_height(status.results_win) / 2
+  local default_speed = vim.api.nvim_win_get_height(status.results_win) - 3
   local speed = status.picker.layout_config.scroll_speed or default_speed
 
   local input = direction > 0 and [[]] or [[]]
